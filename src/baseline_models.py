15882d445984fbb6851cd16e250e943214b4fd55
topsy-13 <santiramir98@gmail.com>
2025-07-14 17:43:26 -0500

    chore: get_baseline from automl

---
+++ src/baseline_models.py
@@ -0,0 +1,30 @@
+import naiveautoml
+import pandas as pd
+
+
+def get_best_models(X, y, n_models=10, 
+                    max_hpo_iterations=100, timeout=60, scoring_metric='neg_log_loss'):
+    naml = naiveautoml.NaiveAutoML(timeout_candidate=timeout, max_hpo_iterations=max_hpo_iterations)
+    naml.fit(X, y)
+    scoreboard = naml.history
+    scoreboard = scoreboard.sort_values(by=scoring_metric, ascending=False).head(n_models)
+    scoreboard = scoreboard[['pipeline', scoring_metric]]
+
+    return scoreboard
+
+
+def get_baseline_metric(scoreboard: pd.DataFrame):
+    baseline_metric = abs(scoreboard.iloc[:,1].mean())
+    return baseline_metric
+
+
+def get_models_and_baseline_metric(X, y, n_models=10, 
+                                    max_hpo_iterations=100, timeout=60, 
+                                    scoring_metric='neg_log_loss'):
+    # Get the best models
+    scoreboard = get_best_models(X, y, n_models, max_hpo_iterations, timeout, scoring_metric)
+    
+    # Get the baseline metric
+    baseline_metric = get_baseline_metric(scoreboard)
+    
+    return baseline_metric
